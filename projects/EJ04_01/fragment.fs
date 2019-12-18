4be5359550d5d53ad287fea172f4489ed5150100
joseba.alonso <jalonso063@gmail.com>
2019-12-18 06:32:35 +0100

    EJ04_01 Done

--- projects/EJ04_01/fragment.fs
+++ projects/EJ04_01/fragment.fs
@@ -1,11 +1,11 @@
 #version 330 core
 
 in vec2 UV;
+in vec3 myColor;
 out vec4 FragColor;
 
 uniform sampler2D tex_1;
-uniform sampler2D tex_2;
 
 void main() {
-    FragColor = mix(texture(tex_1, UV), texture(tex_2, UV), 0.7);
+    FragColor = mix(texture(tex_1, UV), vec4(myColor, 1.0), 0.7);
 }
--- projects/EJ04_01/vertex.vs
+++ projects/EJ04_01/vertex.vs
@@ -5,8 +5,10 @@ layout (location=1) in vec3 aColor;
 layout (location=2) in vec2 aUV;
 
 out vec2 UV;
+out vec3 myColor;
 
 void main() {
     UV = aUV;
+    myColor = aColor;
     gl_Position = vec4(aPos, 1.0);
 }
