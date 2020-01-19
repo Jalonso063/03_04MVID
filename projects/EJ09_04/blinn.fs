93e62c21db939faa7f7c7565845d68afae2a6c70
joseba.alonso <jalonso063@gmail.com>
2020-01-19 16:31:11 +0100

    EJ09_04/blinn.fs completed

--- projects/EJ09_04/blinn.fs
+++ projects/EJ09_04/blinn.fs
@@ -33,8 +33,27 @@ struct PointLight {
     float linear;
     float quadratic;
 };
-#define NUMBER_POINT_LIGHTS 2
+
+struct SpotLight {
+    vec3 position;
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    float cutOff;
+    float outerCutOff;
+};
+#define NUMBER_POINT_LIGHTS 3
 uniform PointLight pointLight[NUMBER_POINT_LIGHTS];
+
+#define NUMBER_SPOT_LIGHTS 3
+uniform SpotLight spotLight[NUMBER_SPOT_LIGHTS];
 
 uniform vec3 viewPos;
 
@@ -71,6 +90,30 @@ vec3 calcPointLight(PointLight light, vec3 normal, vec3 viewDir, vec3 fragPos, v
     return (ambient + diffuse + specular) * attenuation;
 }
 
+vec3 calcSpotLight(SpotLight light, vec3 normal, vec3 viewDir, vec3 fragPos, vec3 albedoMap, vec3 specularMap) {
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant +
+        light.linear * distance +
+        light.quadratic * distance * distance);
+
+    vec3 ambient = albedoMap * light.ambient;
+
+    vec3 norm = normalize(normal);
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * albedoMap * light.diffuse;
+
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float spec = pow(max(dot(norm, halfwayDir), 0.0), material.shininess);
+    vec3 specular = spec * specularMap * light.specular;
+
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+
+    return (ambient + (diffuse * intensity) + (specular * intensity)) * attenuation;
+}
+
 void main() {
     vec3 albedo = vec3(texture(material.diffuse, uv));
     vec3 specular = vec3(texture(material.specular, uv));
@@ -82,6 +125,10 @@ void main() {
 
     for (int i = 0; i < NUMBER_POINT_LIGHTS; ++i) {
      finalColor += calcPointLight(pointLight[i], norm, viewDir, fragPos, albedo, specular);
+	}
+
+    for (int i = 0; i < NUMBER_SPOT_LIGHTS; ++i) {
+     finalColor += calcSpotLight(spotLight[i], norm, viewDir, fragPos, albedo, specular);
 	}
 
     FragColor = vec4(finalColor, 1.0);
