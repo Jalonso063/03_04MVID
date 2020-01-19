c78a81299093b83504d7eaecd95bb8e1842eba1a
joseba.alonso <jalonso063@gmail.com>
2020-01-19 21:40:03 +0100

    WIP EJ10_01

--- projects/EJ10_01/main.cpp
+++ projects/EJ10_01/main.cpp
@@ -15,22 +15,11 @@
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
-glm::vec3 dirLightDirection(-0.2f, -1.0f, -0.3f);
+glm::vec3 dirLightDirection(-0.2f, 0.0f, -0.3f);
 
 glm::vec3 pointLightPositions[] = {
     glm::vec3(4.0f, 2.0f, 0.0f),
     glm::vec3(-4.0f, 2.0f, 0.0f)
-};
-
-glm::vec3 cubePositions[] = {
-    glm::vec3(4.0f, 0.0f, 0.0f),
-    glm::vec3(-4.0f, 0.0f, 0.0f),
-    glm::vec3(0.0f, 0.0f, 4.0f),
-    glm::vec3(0.0f, 0.0f, -4.0f),
-    glm::vec3(4.0f, 0.0f, 4.0f),
-    glm::vec3(4.0f, 0.0f, -4.0f),
-    glm::vec3(-4.0f, 0.0f, 4.0f),
-    glm::vec3(-4.0f, 0.0f, -4.0f),
 };
 
 float lastFrame = 0.0f;
@@ -83,13 +72,51 @@ void onScrollMoved(float x, float y) {
     camera.handleMouseScroll(y);
 }
 
-void render(const Model& object, const Shader& s_phong) {
+void render(const Model& object, const Geometry& light, const Shader& s_phong, const Shader& s_light){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glm::mat4 view = camera.getViewMatrix();
     glm::mat4 proj = glm::perspective(glm::radians(camera.getFOV()), 800.0f / 600.0f, 0.1f, 100.0f);
 
+
+    /*s_light.use();
+    for (uint32_t i = 0; i < pointLightPositions->length(); ++i)
+    {
+        const auto& pointPos = pointLightPositions[i];
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, pointPos);
+        model = glm::scale(model, glm::vec3(0.25f));
+        s_light.set("model", model);
+        s_light.set("view", view);
+        s_light.set("proj", proj);
+        s_light.set("lightColor", 1.0f, 1.0f, 1.0f);
+
+        light.render();
+    }*/
+
+
     s_phong.use();
+    const std::string prefixPoints = "pointLight[";
+    for (uint32_t i = 0; i < pointLightPositions->length(); ++i) {
+        const std::string lightName = prefixPoints + std::to_string(i) + "].";
+
+        s_phong.set((lightName + "position").c_str(), pointLightPositions[i]);
+
+        s_phong.set((lightName + "ambient").c_str(), 0.0f, 0.0f, 0.02f);
+        s_phong.set((lightName + "diffuse").c_str(), 0.0f, 0.0f, 0.2f);
+        s_phong.set((lightName + "specular").c_str(), 0.0f, 0.0f, 0.5f);
+        s_phong.set((lightName + "constant").c_str(), 1.0f);
+        s_phong.set((lightName + "linear").c_str(), 0.09f);
+        s_phong.set((lightName + "quadratic").c_str(), 0.032f);
+    }
+
+    s_phong.set("dirLight.direction", dirLightDirection);
+    s_phong.set("dirLight.ambient", 0.5f, 0.5f, 0.5f);
+    s_phong.set("dirLight.diffuse", 0.5f, 0.5f, 0.5f);
+    s_phong.set("dirLight.specular", 0.5f, 0.5f, 0.5f);
+
+
     glm::mat4 model = glm::mat4(1.0f);
     //model = glm::translate(model, glm::vec3(0.0f, -0.5f, 0.0f));
     //model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
@@ -109,8 +136,11 @@ int main(int, char* []) {
 
     glClearColor(0.0f, 0.3f, 0.6f, 1.0f);
 
-    const Shader s_phong("../projects/EJ10_01/simple.vs", "../projects/EJ10_01/simple.fs");
-    const Model object("../assets/models/formula 1/Formula 1 mesh.obj");
+    const Shader s_phong("../projects/EJ10_01/phong.vs", "../projects/EJ10_01/blinn.fs");
+    const Shader s_light("../projects/EJ10_01/light.vs", "../projects/EJ10_01/light.fs");
+    const Model object("../assets/models/lantern/lantern_obj.obj");
+
+    const Sphere sphere(1.0f, 50, 50);
 
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
@@ -128,7 +158,7 @@ int main(int, char* []) {
         lastFrame = currentFrame;
 
         handleInput(deltaTime);
-        render(object, s_phong);
+        render(object, sphere, s_phong, s_light);
         window->frame();
     }
 
