f869c97bfd3db5b4e7aedc7b20c1e85c6bb7a6c6
joseba.alonso <jalonso063@gmail.com>
2020-01-19 12:41:05 +0100

    EJ09_03 Completed

--- projects/EJ09_03/main.cpp
+++ projects/EJ09_03/main.cpp
@@ -10,11 +10,22 @@
 #include "engine/window.hpp"
 #include "engine/geometry/sphere.hpp"
 #include "engine/geometry/quad.hpp"
+#include "engine/geometry/teapot.hpp"
 
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-glm::vec3 lightPos(0.0f, 2.0f, 0.0f);
+Camera camera(glm::vec3(0.0f, 3.0f, 10.0f));
 glm::vec3 lightDir(0.0f, -1.0f, 0.0f);
-const float k_lightSpeed = 2.5f;
+
+glm::vec3 dirLightDirection(-0.2f, -1.0f, -0.3f);
+
+glm::vec3 pointLightPositions[] = {
+    glm::vec3(4.0f, 2.0f, 0.0f),
+    glm::vec3(-4.0f, 2.0f, 0.0f)
+};
+
+glm::vec3 spotLightPositions[] = {
+    glm::vec3(4.0f, 2.0f, -4.0f),
+    glm::vec3(-4.0f, 2.0f, -4.0f)
+};
 
 glm::vec3 cubePositions[] = {
     glm::vec3(4.0f, 0.0f, 0.0f),
@@ -45,18 +56,6 @@ void handleInput(float dt) {
     }
     if (input->isKeyPressed(GLFW_KEY_D)) {
         camera.handleKeyboard(Camera::Movement::Right, dt);
-    }
-    if (input->isKeyPressed(GLFW_KEY_DOWN)) {
-        lightDir += dt * k_lightSpeed * glm::vec3(0.0f, 0.0f, 1.0f);
-    }
-    if (input->isKeyPressed(GLFW_KEY_UP)) {
-        lightDir -= dt * k_lightSpeed * glm::vec3(0.0f, 0.0f, 1.0f);
-    }
-    if (input->isKeyPressed(GLFW_KEY_LEFT)) {
-        lightDir -= dt * k_lightSpeed * glm::vec3(1.0f, 0.0f, 0.0f);
-    }
-    if (input->isKeyPressed(GLFW_KEY_RIGHT)) {
-        lightDir += dt * k_lightSpeed * glm::vec3(1.0f, 0.0f, 0.0f);
     }
 }
 
@@ -98,18 +97,32 @@ void render(const Geometry& floor, const Geometry& object, const Geometry& light
 
     s_light.use();
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, lightPos);
-    model = glm::scale(model, glm::vec3(0.25f));
-    s_light.set("model", model);
-    s_light.set("view", view);
-    s_light.set("proj", proj);
-    s_light.set("lightColor", 1.0f, 1.0f, 1.0f);
+    for (const auto& pointPos : pointLightPositions) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, pointPos);
+        model = glm::scale(model, glm::vec3(0.25f));
+        s_light.set("model", model);
+        s_light.set("view", view);
+        s_light.set("proj", proj);
+        s_light.set("lightColor", 1.0f, 0.0f, 0.0f);
 
-    light.render();
+        light.render();
+    }
+
+    for (const auto& pointPos : spotLightPositions) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, pointPos);
+        model = glm::scale(model, glm::vec3(0.25f));
+        s_light.set("model", model);
+        s_light.set("view", view);
+        s_light.set("proj", proj);
+        s_light.set("lightColor", 1.0f, 1.0f, 1.0f);
+
+        light.render();
+    }
 
     s_phong.use();
-    model = glm::mat4(1.0f);
+    glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3(0.0f, -0.5f, 0.0f));
     model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     model = glm::scale(model, glm::vec3(10.0f, 10.0f, 10.0f));
@@ -122,16 +135,37 @@ void render(const Geometry& floor, const Geometry& object, const Geometry& light
 
     s_phong.set("viewPos", camera.getPosition());
 
-    s_phong.set("light.position", lightPos);
-    s_phong.set("light.direction", lightDir);
-    s_phong.set("light.ambient", 0.1f, 0.1f, 0.1f);
-    s_phong.set("light.diffuse", 0.5f, 0.5f, 0.5f);
-    s_phong.set("light.specular", 1.0f, 1.0f, 1.0f);
-    s_phong.set("light.constant", 1.0f);
-    s_phong.set("light.linear", 0.2f);
-    s_phong.set("light.quadratic", 0.06f);
-    s_phong.set("light.cutOff", glm::cos(glm::radians(40.0f)));
-    s_phong.set("light.outerCutOff", glm::cos(glm::radians(50.0f)));
+    s_phong.set("dirLight.direction", dirLightDirection);
+    s_phong.set("dirLight.ambient", 0.02f, 0.02f, 0.02f);
+    s_phong.set("dirLight.diffuse", 0.1f, 0.1f, 0.1f);
+    s_phong.set("dirLight.specular", 0.2f, 0.2f, 0.2f);
+
+    const std::string prefixPoints = "pointLight[";
+    const std::string prefixSpotPoints = "spotLight[";
+    for (uint32_t i = 0; i < sizeof(pointLightPositions); ++i) {
+        const std::string lightName = prefixPoints + std::to_string(i) + "].";
+        s_phong.set((lightName + "position").c_str(), pointLightPositions[i]);
+        s_phong.set((lightName + "ambient").c_str(), 0.02f, 0.02f, 0.02f);
+        s_phong.set((lightName + "diffuse").c_str(), 0.2f, 0.0f, 0.0f);
+        s_phong.set((lightName + "specular").c_str(), 0.5f, 0.0f, 0.0f);
+        s_phong.set((lightName + "constant").c_str(), 1.0f);
+        s_phong.set((lightName + "linear").c_str(), 0.09f);
+        s_phong.set((lightName + "quadratic").c_str(), 0.032f);
+    }
+
+    for (uint32_t i = 0; i < sizeof(spotLightPositions); ++i) {
+        const std::string lightName = prefixSpotPoints + std::to_string(i) + "].";
+        s_phong.set((lightName + "position").c_str(), spotLightPositions[i]);
+        s_phong.set((lightName + "direction").c_str(), lightDir);
+        s_phong.set((lightName + "ambient").c_str(), 0.02f, 0.02f, 0.02f);
+        s_phong.set((lightName + "diffuse").c_str(), 0.2f, 0.2f, 0.2f);
+        s_phong.set((lightName + "specular").c_str(), 0.5f, 0.5f, 0.5f);
+        s_phong.set((lightName + "constant").c_str(), 1.0f);
+        s_phong.set((lightName + "linear").c_str(), 0.09f);
+        s_phong.set((lightName + "quadratic").c_str(), 0.032f);
+        s_phong.set((lightName + "cutOff").c_str(), glm::cos(glm::radians(10.0f)));
+        s_phong.set((lightName + "outerCutOff").c_str(), glm::cos(glm::radians(15.0f)));
+    }
 
     t_albedo.use(s_phong, "material.diffuse", 0);
     t_specular.use(s_phong, "material.specular", 1);
@@ -142,6 +176,9 @@ void render(const Geometry& floor, const Geometry& object, const Geometry& light
     for (const auto& cubePos : cubePositions) {
         model = glm::mat4(1.0f);
         model = glm::translate(model, cubePos);
+        model = glm::translate(model, glm::vec3(0.0f, -0.5f, 0.0f));
+        model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
         s_phong.set("model", model);
 
         glm::mat3 normalMat = glm::inverse(glm::transpose(glm::mat3(model)));
@@ -161,7 +198,7 @@ int main(int, char* []) {
     const Texture t_albedo("../assets/textures/bricks_albedo.png", Texture::Format::RGB);
     const Texture t_specular("../assets/textures/bricks_specular.png", Texture::Format::RGB);
     const Sphere sphere(1.0f, 50, 50);
-    const Cube cube(1.0f);
+    const Teapot teapot(20);
     const Quad quad(1.0f);
 
     Texture tex("../assets/textures/blue_blocks.jpg", Texture::Format::RGB);
@@ -182,7 +219,7 @@ int main(int, char* []) {
         lastFrame = currentFrame;
 
         handleInput(deltaTime);
-        render(quad, cube, sphere, s_phong, s_light, t_albedo, t_specular);
+        render(quad, teapot, sphere, s_phong, s_light, t_albedo, t_specular);
         window->frame();
     }
 
